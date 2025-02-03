22a68c0f50e1256f6cf8eb8f139d99ecdb222512
Gopesh Sharma <gopesh.sharma@gmail.com>
2025-02-03 03:35:20 -0500

    some modifications

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ConditionsService } from '../../core/services/conditions.service';
 import { WeatherService } from '../../core/services/weather.service';
 
 @Component({
@@ -9,10 +10,11 @@ import { WeatherService } from '../../core/services/weather.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  conditionsData: any;
   city: string = 'london';
   weatherData: any;
   location = '';
-  weatherIcon = 'mostly-sunny.svg';
+  weatherIcon = 'Group1.svg';
   currentDate = 'Tuesday 30 August';
   summary = 'Mostly Sunny';
   displayCity: string = '';
@@ -22,21 +24,25 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.checkWeather();
   }
-  constructor(private service: WeatherService) {}
+  constructor(
+    private service: WeatherService,
+    private conditionService: ConditionsService
+  ) {
+    this.conditionsData = this.conditionService.getConditions();
+    console.log(this.conditionsData);
+  }
   setCity(event: any) {
     this.city = event.target.value;
-    if (this.city == '') {
-      this.city = 'london';
-    }
   }
   checkWeather() {
     this.weatherData = this.service.getWeather(this.city).subscribe((data) => {
       console.log(data);
       this.displayCity = `${data.location.name},`;
-      this.displayRegionCountry = `${data.location.country}`;
+      this.displayRegionCountry = `${data.location.region}, ${data.location.country}`;
       this.tempInCentigrade = `${data.current.temp_c}`;
       this.summary = `${data.current.condition.text}`;
       this.lastUpdated = `${data.current.last_updated}`;
+      //this.weatherIcon = `${data.current.condition.icon}`;
     });
   }
 }
