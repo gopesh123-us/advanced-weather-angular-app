1d20cecf93f54c8911c251972b08b9598373e9f2
Gopesh Sharma <gopesh.sharma@gmail.com>
2025-02-03 19:24:28 -0500

    conditionally changes the background and few icons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,30 @@
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule, DatePipe, NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCloudMoon } from '@fortawesome/free-solid-svg-icons';
 import { ConditionsService } from '../../core/services/conditions.service';
 import { WeatherService } from '../../core/services/weather.service';
 
 @Component({
   selector: 'app-home',
-  imports: [DatePipe, CommonModule],
+  imports: [DatePipe, CommonModule, FontAwesomeModule, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  faCloudMoon = faCloudMoon;
   conditionsData: any;
   city: string = 'london';
   weatherData: any;
   location = '';
-  weatherIcon = 'Group1.svg';
+  weatherIcon = '';
   currentDate = 'Tuesday 30 August';
   summary = 'Mostly Sunny';
   displayCity: string = '';
   displayRegionCountry: string = '';
   tempInCentigrade: string = '';
   lastUpdated: string = '';
+  isDay: number = 0;
   ngOnInit(): void {
     this.checkWeather();
   }
@@ -42,7 +46,32 @@ export class HomeComponent implements OnInit {
       this.tempInCentigrade = `${data.current.temp_c}`;
       this.summary = `${data.current.condition.text}`;
       this.lastUpdated = `${data.current.last_updated}`;
-      //this.weatherIcon = `${data.current.condition.icon}`;
+      this.weatherIcon = this.getIcon(
+        data.current.is_day,
+        data.current.condition.code
+      );
+      this.isDay = data.current.is_day;
     });
+  }
+  getIcon(day: number, code: number) {
+    if (day === 1 && code === 1000) {
+      return '1000-d-sunny.svg';
+    } else if (day === 0 && code === 1000) {
+      return '1000-n-clear.svg';
+    } else if (day === 1 && code === 1003) {
+      return '1003-d-partly-cloudy.png';
+    } else if (day === 0 && code === 1003) {
+      return '1003-n-partly-cloudy.png';
+    } else if (day === 1 && code === 1030) {
+      return '1030-d-mist.png';
+    } else if (day === 0 && code === 1030) {
+      return '1030-n-mist.svg';
+    } else if (day === 0 && code == 1213) {
+      return '1213-d-light-snow.png';
+    } else if (day === 0 && code === 1213) {
+      return '1213-n-light-snow-png';
+    } else {
+      return 'icon-not available';
+    }
   }
 }
